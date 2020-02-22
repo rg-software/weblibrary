90aa6b00f83ce0505bd833fdb862d01fc347b362
Maxim Mozgovoy <rgsoftware@gmail.com>
2020-02-23 03:32:39 +0900

    WebLibrary Downloader project added

---
+++ WebLibraryDownloader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebLibraryDownloader
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            MainForm f = new MainForm();
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length >= 2)
+                f.SetUrl(args[1]);
+            
+            Application.Run(f);
+        }
+    }
+}
